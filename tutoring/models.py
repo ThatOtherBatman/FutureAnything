c65c504e27138414dd0698bb3cb22a58ca321741
Rohan Dalton <rohan.dalton@gmail.com>
2017-12-18 00:51:11 -0500

    Added students as model.

--- tutoring/models.py
+++ tutoring/models.py
@@ -134,6 +134,64 @@ class TeacherRegistration(models.Model):
         return f"TeacherRegistration({self.tutor}, {self.expiry})"
 
 
+class Student(models.Model):
+
+    KINDERGARTEN = "K"
+    Y1 = "1"
+    Y2 = "2"
+    Y3 = "3"
+    Y4 = "4"
+    Y5 = "5"
+    Y6 = "6"
+    Y7 = "7"
+    Y8 = "8"
+    Y9 = "9"
+    Y10 = "10"
+    Y11 = "11"
+    Y12 = "12"
+
+    YEAR_CHOICES = (
+        (KINDERGARTEN, "Kindergarten"),
+        (Y1, "Year 1"),
+        (Y2, "Year 2"),
+        (Y3, "Year 3"),
+        (Y4, "Year 4"),
+        (Y5, "Year 5"),
+        (Y6, "Year 6"),
+        (Y7, "Year 7"),
+        (Y8, "Year 8"),
+        (Y9, "Year 9"),
+        (Y10, "Year 10"),
+        (Y11, "Year 11"),
+        (Y12, "Year 12"),
+    )
+
+    first_name = models.CharField(max_length=100,
+                                  null=False,
+                                  )
+    middle_name = models.CharField(max_length=100,
+                                   null=True,
+                                   blank=True,
+                                   default=None,
+                                   )
+    family_name = models.CharField(max_length=100,
+                                   null=False,
+                                   )
+    year = models.CharField(choices=YEAR_CHOICES,
+                            null=False,
+                            max_length=2,
+                            )
+    date_of_birth = models.DateField(null=False)
+    email = models.EmailField(null=False,
+                              unique=True,)
+
+    class Meta(object):
+        ordering = ("first_name", "family_name")
+
+    def __str__(self):
+        return f"{self.first_name} {self.family_name}"
+
+
 class Subject(models.Model):
 
     name = models.CharField(max_length=50,
